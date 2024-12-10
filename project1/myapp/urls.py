6d3fd2fdcf6b95e7ba06354a203d779c0423897e
Iskandar <dexqonoviskandar104@gmail.com>
2024-12-10 10:59:04 +0500

    projekt chiqarilfi

---
+++ project1/myapp/urls.py
@@ -0,0 +1,20 @@
+from . import views
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('view1/', views.view1, name='view1'),
+    path('view2/', views.view2, name='view2'),
+    path('view3/', views.view3, name='view3'),
+    path('view4/', views.view4, name='view4'),
+    path('view5/', views.view5, name='view5'),
+    path('view6/', views.view6, name='view6'),
+    path('view7/', views.view7, name='view7'),
+    path('view8/', views.view8, name='view8'),
+    path('view9/', views.view9, name='view9'),
+    path('view10/', views.view10, name='view10'),
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+
+]
+
+
---
+++ project1/myapp/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render
+
+def view1(request):
+    return render(request, 'myapp/view1.html')
+
+def view2(request):
+    return render(request, 'myapp/view2.html')
+
+def view3(request):
+    return render(request, 'myapp/view3.html')
+
+def view4(request):
+    return render(request, 'myapp/view4.html')
+
+def view5(request):
+    return render(request, 'myapp/view5.html')
+
+def view6(request):
+    return render(request, 'myapp/view6.html')
+
+def view7(request):
+    return render(request, 'myapp/view7.html')
+
+def view8(request):
+    return render(request, 'myapp/view8.html')
+
+def view9(request):
+    return render(request, 'myapp/view9.html')
+
+def view10(request):
+    return render(request, 'myapp/view10.html')
